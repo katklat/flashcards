a5350124bb20d9633159f1c2eeee638e6b9d1620
Jeremias Erbs <jerry@badabam.de>
2019-09-23 16:09:31 +0200

    Add useEffect to fetch and post new cards

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Navigation from './Navigation'
-import HomePage from './HomePage'
+import CardList from './CardList'
 import styled from 'styled-components/macro'
 import SettingsPage from './SettingsPage'
+import { getAllCards, postCard } from './services'
 
 export default function App() {
+  useEffect(() => {
+    getAllCards().then(setCards)
+  }, [])
+
   const [activeIndex, setActiveIndex] = useState(0)
-  const [cards, setCards] = useState([
-    {
-      title: 'Foo',
-      question: 'What?',
-      answer: 'That!',
-    },
-    {
-      title: 'Bar',
-      question: 'This?',
-      answer: 'That!',
-    },
-  ])
+  const [cards, setCards] = useState([])
 
   function createCard(cardData) {
-    console.log(cardData)
+    postCard(cardData).then(card => setCards([...cards, card]))
   }
 
   function renderPage() {
     const pages = {
-      0: <HomePage cards={cards} />,
-      1: <section>Practice</section>,
-      2: <section>Bookmarks</section>,
+      0: <CardList title="Home" cards={cards} />,
+      1: <CardList title="Practice" cards={cards.filter(card => card.doPractice)} />,
+      2: <CardList title="Bookmarks" cards={cards.filter(card => card.isBookmarked)} />,
       3: <SettingsPage onSubmit={createCard} />,
     }
 
--- src/CardList.js
+++ src/CardList.js
@@ -2,10 +2,10 @@ import React from 'react'
 import Card from './Card'
 import styled from 'styled-components/macro'
 
-export default function HomePage({ cards }) {
+export default function CardList({ cards, title }) {
   return (
     <PageStyled>
-      <h1>Homepage</h1>
+      <h1>{title}</h1>
       {cards.map((card, index) => (
         <Card key={index} title={card.title} question={card.question} answer={card.answer} />
       ))}
