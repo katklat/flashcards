2b8588cf919127230e993e763297035b40727077
Jeremias Erbs <jerry@badabam.de>
2019-09-25 09:48:56 +0200

    Add highlight.js for syntax highlighting

--- src/common/Markdown.js
+++ src/common/Markdown.js
@@ -1,8 +1,20 @@
 import MarkdownIt from 'markdown-it'
 import React from 'react'
 import styled from 'styled-components/macro'
+import * as hljs from 'highlight.js'
+import 'highlight.js/styles/github.css'
 
-const md = new MarkdownIt()
+const md = new MarkdownIt({
+  highlight: function(str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value
+      } catch (__) {}
+    }
+
+    return '' // use external default escaping
+  },
+})
 export default function Markdown({ children, font = 'serif' }) {
   return <DivStyled {...{ font }} dangerouslySetInnerHTML={{ __html: md.render(children) }}></DivStyled>
 }
