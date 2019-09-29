6f815376cc53ebbccec7a744c848893fcae665fe
elenasmo <elena.smo@web.de>
2019-09-29 11:30:05 +0200

    Project setup

--- src/cards/Card.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Card from './Card'
-
-export default {
-  title: 'Card',
-}
-
-export const card = () => <Card title="test" question="test" answer="test" tags={['one', 'two']} />
--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './app/App'
-import GlobalStyles from './common/GlobalStyles'
+import App from './App'
 import * as serviceWorker from './serviceWorker'
 
 const rootElement = document.getElementById('root')
 ReactDOM.render(
   <>
-    <GlobalStyles />
     <App />
   </>,
   rootElement
