954376a6ebbf4b9c8573609b2df70076f13b23a0
Jeremias Erbs <jerry@badabam.de>
2019-12-03 12:43:12 +0100

    Cleanup

--- cypress/integration/Settings.spec.js
+++ cypress/integration/Settings.spec.js
@@ -1,53 +1,53 @@
 /// <reference types="Cypress" />
 
-context('Settings', () => {
+context("Settings", () => {
   beforeEach(() => {
-    cy.visit('/settings')
-  })
+    cy.visit("/settings");
+  });
 
-  it('has the Settings header', () => {
-    cy.get('header').should('contain', 'Settings')
-  })
+  it("has the Settings header", () => {
+    cy.get("header").should("contain", "Settings");
+  });
 
-  it('has 2 inputs and 2 text areas and one button', () => {
-    cy.get('input').should('have.length', 2)
-    cy.get('textarea').should('have.length', 2)
-    cy.get('button')
-      .contains('Create')
-      .should('have.length', 1)
-  })
+  it("has 2 inputs and 2 text areas and one button", () => {
+    cy.get("input").should("have.length", 2);
+    cy.get("textarea").should("have.length", 2);
+    cy.get("button")
+      .contains("Create")
+      .should("have.length", 1);
+  });
 
-  it.only('creates a new card', () => {
-    cy.get('label')
-      .contains('Title')
-      .find('input')
-      .type('TestHeadline')
+  it("creates a new card", () => {
+    cy.get("label")
+      .contains("Title")
+      .find("input")
+      .type("TestHeadline");
 
-    cy.get('label')
-      .contains('Question')
-      .find('textarea')
-      .type('TestQuestion')
+    cy.get("label")
+      .contains("Question")
+      .find("textarea")
+      .type("TestQuestion");
 
-    cy.get('label')
-      .contains('Answer')
+    cy.get("label")
+      .contains("Answer")
       .parent()
-      .find('textarea')
-      .type('TestAnswer')
+      .find("textarea")
+      .type("TestAnswer");
 
-    cy.get('label')
-      .contains('Tags')
-      .find('input')
-      .type('test, tag')
+    cy.get("label")
+      .contains("Tags")
+      .find("input")
+      .type("test, tag");
 
-    cy.get('button')
-      .contains('Create card')
-      .click()
+    cy.get("button")
+      .contains("Create card")
+      .click();
 
-    cy.visit('/')
-    cy.get('h2').contains('TestHeadline')
+    cy.visit("/");
+    cy.get("h2").contains("TestHeadline");
 
-    cy.request('http://localhost:3333/cards/delete-test-cards').then(res => {
-      cy.wrap(res.body.success).should('be.true')
-    })
-  })
-})
+    cy.request("http://localhost:3333/cards/delete-test-cards").then(res => {
+      cy.wrap(res.body.success).should("be.true");
+    });
+  });
+});
