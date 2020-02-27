b6f5f6eeeffbbf172577ecdcb12aa2e289881167
Jeremias Erbs <jerry@badabam.de>
2020-02-27 19:25:15 +0100

    Fix errors and status on server

--- server/routes/cards.js
+++ server/routes/cards.js
@@ -3,40 +3,40 @@ import Card from '../models/Card'
 
 const router = Router()
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   Card.find()
     .then(cards => res.json(cards))
-    .catch(err => res.json(err))
+    .catch(next)
 })
 
-router.get('/:id', (req, res) => {
-  Card.find({ id: req.params.id })
-    .then(cards => res.json(cards))
-    .catch(err => res.json(err))
+router.get('/:id', (req, res, next) => {
+  Card.findById(req.params.id)
+    .then(card => (card ? res.json(card) : res.sendStatus(404)))
+    .catch(next)
 })
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
   Card.create(req.body)
-    .then(card => res.json(card))
-    .catch(err => res.json(err))
+    .then(card => res.status(201).json(card))
+    .catch(next)
 })
 
-router.patch('/:id', (req, res) => {
+router.patch('/:id', (req, res, next) => {
   Card.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    .then(card => res.json(card))
-    .catch(err => res.json(err))
+    .then(card => (card ? res.json(card) : res.sendStatus(404)))
+    .catch(next)
 })
 
-router.delete('/:id', (req, res) => {
-  Card.findByIdAndDelete(req.params.id)
-    .then(card => res.json(card))
-    .catch(err => res.json(err))
-})
-
-router.delete('/all', (req, res) => {
+router.delete('/all', (req, res, next) => {
   Card.deleteMany()
-    .then(card => res.json(card))
-    .catch(err => res.json(err))
+    .then(() => res.sendStatus(200))
+    .catch(next)
+})
+
+router.delete('/:id', (req, res, next) => {
+  Card.findByIdAndDelete(req.params.id)
+    .then(card => (card ? res.json(card) : res.sendStatus(404)))
+    .catch(next)
 })
 
 export default router
