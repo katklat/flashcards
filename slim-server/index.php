c6ad37afa9d429c870c1640785096b1ffc6620e3
Till Schander <Braunbart@users.noreply.github.com>
2020-03-20 11:43:03 +0100

    Added slim server

---
+++ slim-server/index.php
@@ -0,0 +1,34 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Middlewares\TrailingSlash;
+
+require __DIR__ . '/../../slim-server/vendor/autoload.php';
+
+$app = AppFactory::create();
+$app->add(new TrailingSlash(false)); // remove trailing slashes
+
+// GET /
+$app->get('/', function (Request $request, Response $response, array $args) {
+    // TODO serve the html for the react app instead of "Hello world!"
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+// GET /cards
+
+// GET /cards/{id}
+
+// POST /cards
+
+// PATCH /cards/{id}
+
+// DELETE /cards/all
+
+// DELETE /cards/{id}
+
+
+$app->addErrorMiddleware(true, false, false);
+$app->run();
